f386b468aec42e40c87aae0fc252445533a287dc
Aman <amangarg0303@gmail.com>
2024-01-26 00:55:35 +0530

    Genres section

---
+++ components/Genres.tsx
@@ -0,0 +1,28 @@
+const genres = [
+  "Comedy",
+  "Drama",
+  "Horror",
+  "Thriller",
+  "Mystery",
+  "Action",
+  "Sci-fic",
+  "Documentry",
+  "Fantasy",
+  "Adventure",
+  "Short Story",
+];
+
+export const Genres = () => {
+  return (
+    <div className="flex gap-x-24 mx-16 px-16 py-4 no-scrollbar overflow-x-auto">
+      {genres.map((genre, _) => (
+        <li
+          key={_}
+          className="list-none whitespace-nowrap border px-8 py-4 text-xl font-semibold rounded-xl cursor-pointer bg-[#0e0e0e] transition duration-100 transform hover:scale-125"
+        >
+          {genre}
+        </li>
+      ))}
+    </div>
+  );
+};
