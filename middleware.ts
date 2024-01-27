1829e83f17f12ebe3708c055bb9d8414b1aa48ab
Aman <amangarg0303@gmail.com>
2024-01-27 18:42:37 +0530

    secure admin route

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,34 @@
 import NextAuth from "next-auth";
 import authConfig from "@/auth.config";
+import { getToken } from "next-auth/jwt";
 
 const { auth } = NextAuth(authConfig);
 
 const publicRoutes: Array<String> = [];
 const authRoutes = ["/auth/login"];
 const apiAuthPrefix = "/api/auth";
+const adminRoutes = ["/admin/addMovie"];
 
 export const DEFAULT_LOGIN_REDIRECT = "/";
 
-export default auth((req) => {
+export default auth(async (req) => {
   const { nextUrl } = req;
   const isLoggedIn = !!req.auth;
   const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
   const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
   const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+  const isAdminRoute = nextUrl.pathname.startsWith("/admin");
+
+  // console.log(req.auth);
+  const token = await getToken({ req, secret: process.env.AUTH_SECRET });
+  const isAdmin = token?.role === "ADMIN";
+
+  if (isAdminRoute) {
+    if (!isAdmin) {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+    }
+  }
+
   if (isApiAuthRoute) {
     return null;
   }
