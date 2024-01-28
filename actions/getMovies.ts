8bddd2669528f5effd068b041c78a6ec9165af62
Aman <amangarg0303@gmail.com>
2024-01-28 15:11:01 +0530

    cache all movies

--- actions/getMovies.ts
+++ actions/getMovies.ts
@@ -1,10 +1,18 @@
 "use server";
 
 import prisma from "@/lib/db";
+import redis from "@/lib/redisClient";
 
 export const getMoviesAction = async () => {
   try {
+    const cacheVal = await redis.get("movies");
+    if (cacheVal) {
+      return JSON.parse(cacheVal);
+    }
+
     const movies = await prisma.movie.findMany({});
+    await redis.set("movies", JSON.stringify(movies));
+
     return movies;
   } catch (error) {
     return [];
