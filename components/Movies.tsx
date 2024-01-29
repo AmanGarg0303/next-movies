03d7c5937d77ee8fe501fb4508e2be567efc6611
Aman <amangarg0303@gmail.com>
2024-01-29 23:02:04 +0530

    remove console

--- components/Movies.tsx
+++ components/Movies.tsx
@@ -3,7 +3,6 @@ import { Movie } from "@/components/Movie";
 
 export const Movies = async () => {
   const movies: Array<IMovie> = await getMoviesAction();
-  console.log("helo");
 
   return (
     <div className="grid grid-cols-5 gap-x-16 gap-y-20 px-16 py-8 ">
