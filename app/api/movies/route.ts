df21475e391d2994c6dfbb180eb86dbd623b19f0
Aman <amangarg0303@gmail.com>
2024-01-31 00:49:20 +0530

    caching data using redis

--- app/api/movies/route.ts
+++ app/api/movies/route.ts
@@ -1,5 +1,6 @@
 import prisma from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
+import redis from "@/lib/redisClient";
 
 export async function GET(req: NextRequest) {
   const page: number = Number(req.nextUrl.searchParams.get("page")) || 1;
@@ -7,31 +8,40 @@ export async function GET(req: NextRequest) {
 
   const skip = (page - 1) * limit;
 
+  const query = "movies/page:" + page + "limit:" + limit;
+
   try {
+    const cacheVal = await redis.get(query);
     const totalMovies = await prisma.movie.count();
     const totalPages = Math.ceil(totalMovies / limit);
 
-    const movies = await prisma.movie.findMany({
-      take: limit,
-      skip,
-      orderBy: {
-        createdAt: "desc",
-      },
-      select: {
-        id: true,
-        title: true,
-        rating: true,
-        coverImg: true,
-        genres: true,
-        publishedDate: true,
-      },
-    });
+    if (cacheVal) {
+      let data = JSON.parse(cacheVal);
+      return NextResponse.json({ status: 200, data: data, totalPages });
+    } else {
+      const movies = await prisma.movie.findMany({
+        take: limit,
+        skip,
+        orderBy: {
+          createdAt: "desc",
+        },
+        select: {
+          id: true,
+          title: true,
+          rating: true,
+          coverImg: true,
+          genres: true,
+          publishedDate: true,
+        },
+      });
+      await redis.set(query, JSON.stringify(movies));
 
-    return NextResponse.json({
-      status: 200,
-      data: movies,
-      totalPages,
-    });
+      return NextResponse.json({
+        status: 200,
+        data: movies,
+        totalPages,
+      });
+    }
   } catch (error) {
     return NextResponse.json({ status: 400, error: error });
   }
